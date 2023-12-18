0df98f9029d532914398a19ae1dbe51408027a2b
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-12-18 22:38:01 +0100

    fix: example of tenant is invalid

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -12,11 +12,11 @@ provider "kestra" {
   jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Iktlc3RyYS5pbyIsImlhdCI6MTUxNjIzOTAyMn0.hm2VKztDJP7CUsI69Th6Y5NLEQrXx7OErLXay55GD5U"
 
   # optional tenant id (EE)
-  tenant_id = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Iktlc3RyYS5pbyIsImlhdCI6MTUxNjIzOTAyMn0.hm2VKztDJP7CUsI69Th6Y5NLEQrXx7OErLXay55GD5U"
+  tenant_id = "the-tenant-id"
 
   # optional extra headers
   extra_headers = {
-    x-pipeline    = "*****"
+    x-pipeline = "*****"
     authorization = "Bearer *****"
   }
 }

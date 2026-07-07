3edb1e2e1a54fa994573b80849b3c94ee57db604
Malay Dewangan <malaydewangan310@gmail.com>
2026-07-07 12:30:42 +0530

    feat: support email lookup in user data source

--- internal/provider/data_source_user_test.go
+++ internal/provider/data_source_user_test.go
@@ -34,3 +34,38 @@ func testAccDataSourceUser(id string) string {
 		id,
 	)
 }
+
+func TestAccDataSourceUserByEmail(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceUserByEmail("data-source-email@example.com"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.kestra_user.by_email", "user_id",
+						"kestra_user.email_lookup", "id",
+					),
+					resource.TestCheckResourceAttr(
+						"data.kestra_user.by_email", "email", "data-source-email@example.com",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceUserByEmail(email string) string {
+	return fmt.Sprintf(
+		`
+        resource "kestra_user" "email_lookup" {
+            email = "%s"
+        }
+
+        data "kestra_user" "by_email" {
+            email = kestra_user.email_lookup.email
+        }`,
+		email,
+	)
+}

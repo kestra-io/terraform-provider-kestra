3a2ca5a500bf027a6682cb367353ebbae0e516f2
Malay Dewangan <malaydewangan310@gmail.com>
2026-07-13 11:54:28 +0530

    feat: support name lookup in role data source

--- internal/provider/data_source_role_test.go
+++ internal/provider/data_source_role_test.go
@@ -34,3 +34,43 @@ func testAccDataSourceRole(id string) string {
 		id,
 	)
 }
+
+func TestAccDataSourceRoleByName(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceRoleByName("data-source-role-lookup"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.kestra_role.by_name", "role_id",
+						"kestra_role.name_lookup", "id",
+					),
+					resource.TestCheckResourceAttr(
+						"data.kestra_role.by_name", "name", "data-source-role-lookup",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceRoleByName(name string) string {
+	return fmt.Sprintf(
+		`
+        resource "kestra_role" "name_lookup" {
+            name = "%s"
+            resources {
+                type = "FLOW"
+                actions = ["VIEW", "LIST"]
+            }
+            is_default = false
+        }
+
+        data "kestra_role" "by_name" {
+            name = kestra_role.name_lookup.name
+        }`,
+		name,
+	)
+}
